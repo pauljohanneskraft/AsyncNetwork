bd530ccb959edd978d163b9dcb0308aa7966450d
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2022-07-14 13:48:19 +0200

    Add task cancellation to download task

--- Sources/URLSession+Async.swift
+++ Sources/URLSession+Async.swift
@@ -19,16 +19,22 @@ extension URLSession {
         if #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) {
             return try await self.data(for: request)
         } else {
-            return try await withCheckedThrowingContinuation { continuation in
-                let task = self.dataTask(with: request) { data, response, error in
-                    guard let data = data, let response = response, error == nil else {
-                        precondition(error != nil, "There should either be data with a response or an error.")
-                        continuation.resume(throwing: error ?? URLError(.badServerResponse))
-                        return
+            var task: URLSessionDataTask?
+            let onCancel = { task?.cancel() }
+            return try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    task = self.dataTask(with: request) { data, response, error in
+                        guard let data = data, let response = response, error == nil else {
+                            precondition(error != nil, "There should either be data with a response or an error.")
+                            continuation.resume(throwing: error ?? URLError(.badServerResponse))
+                            return
+                        }
+                        continuation.resume(returning: (data, response))
                     }
-                    continuation.resume(returning: (data, response))
+                    task?.resume()
                 }
-                task.resume()
+            } onCancel: {
+                onCancel()
             }
         }
     }
